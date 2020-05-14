6d7c4c402110b980d9fb64fe06e27cb4cf1a419f
adrianomartinelli <adrianom@student.ethz.ch>
2020-05-15 00:09:40 +0200

    Baseline neural net

--- main.py
+++ main.py
@@ -23,6 +23,8 @@ from sklearn.model_selection import ParameterGrid
 from sklearn.model_selection import StratifiedKFold, KFold
 
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_auc_score, precision_recall_curve, auc
+
+import tensorflow as tf
 #%%
 
 # Global variables
@@ -154,12 +156,86 @@ results['xgboost'] = model_evaluation(xgboost, list(ParameterGrid(parameters['xg
 # ## SVC
 results['svc'] = model_evaluation(svc, list(ParameterGrid(parameters['svc'])), 'svc', cv=5)
 
+# #Further models to test
+# * Naive Bayes
+# * Logistic Regression
 
 
+#%%
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Dense
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import OneHotEncoder
 
+def prep_data_nn(X,y):
+    le = LabelEncoder()
+    le.fit(nuc)
+    X = X.apply(lambda x: le.transform([i for i in x]))
+    X = pd.DataFrame([*X])
+    
+    y[y==-1] = 0
+    
+    return X.values, y.values
 
+def prep_data_cnn(X,y):
+    ohe = OneHotEncoder()
+    ohe.fit(np.array(nuc).reshape(-1,1))
+    
+    #Split sequence in list of letters
+    tmp = X.apply(lambda x: [i for i in x])
+    
+    #Convert to array
+    tmp = np.array([*tmp])
+    
+    #Encode each sequence (2D array) and store as list of arrays
+    tmp = [ohe.transform(i.reshape(-1,1)).A for i in tmp]
+    
+    #Reshape to prepare for concat, i.e. create shape (1,82,4)
+    tmp = [i.reshape(1,-1,4) for i in tmp]
+    
+    #Concat along axis=0
+    tmp = np.concatenate(tmp, axis=0)
+    
+    y[y==-1] = 0
+    
+    return tmp, y.values
 
+#%%
+# # Basline NN
+def nn_sequential(hidden, output=1, hidden_activation='relu',output_activation='sigmoid'):
+    
+    model = Sequential()
+    model.add(Dense(hidden[0], input_shape = (82,), activation=hidden_activation))
+    
+    for i in hidden[1:]:
+        model.add(Dense(i, activation=hidden_activation))
+        
+    model.add(Dense(output, activation=output_activation))
+    return model
 
+# # Convolutional NN
+
+#%%
+from tensorflow.keras.optimizers import SGD
+from tensorflow.keras.losses import BinaryCrossentropy
+
+# # NN
+x_prep, y_prep = prep_data_nn(x_train, y_train)
+
+BATCH_SIZE = 10
+EPOCHS=3
+
+model = nn_sequential([128,64,32])
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+model.fit(x_prep, y_prep, batch_size=BATCH_SIZE, epochs=EPOCHS)
+
+x_prep, y_prep = prep_data_nn(x_test, y_test)
+pred = model.predict(x_prep)
+pred[pred >= 0.5] = 1
+pred[pred < 0.5] = 0
+#Compute scores
+score = [m(y_prep, pred) for m in metrics]
 
 
 
